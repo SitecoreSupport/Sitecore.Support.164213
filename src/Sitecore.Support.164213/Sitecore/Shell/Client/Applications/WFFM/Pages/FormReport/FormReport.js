269381b5802f90a3435b1f74b87cfb514ffb3a37
DK\NPA <npa@sitecore.net>
2017-05-26 19:06:37 +0300

    Initial patch

---
+++ src/Sitecore.Support.164213/Sitecore/Shell/Client/Applications/WFFM/Pages/FormReport/FormReport.js
@@ -0,0 +1,52 @@
+﻿define(["sitecore"], function (sc) {
+
+    var self;
+
+    var app = sc.Definitions.App.extend({
+        initialized: function () {
+            self = this;
+
+            this.FormFieldsBarChart.on("segmentSelected", this.selectSegment);
+        },
+
+        exportToExcel: function () {
+            this.exportData(1);
+        },
+
+        exportToXml: function () {
+            this.exportData(0);
+        },
+
+        selectSegment: function (obj) {
+            self.trigger("close:DetailedFieldsReportSmartPanel");
+            var dataSourceItems = self.FormResponsesDataSource.attributes.data;
+
+            var listDetails = dataSourceItems[obj.id];
+            if (listDetails == null) {
+                return;
+            }
+            self.FormResponsesDataSource.set("selectedSegmentData", listDetails);
+
+            self.trigger("open:DetailedFieldsReportSmartPanel");
+        },
+
+        exportData: function (format) {
+            var params = sc.Helpers.url.getQueryParameters(window.location.href);
+            var id = this.get("itemId") || params.itemId || this.get("formId") || params.formId || this.get("id") || params.id;
+
+            $.ajax({
+                type: 'POST',
+                //Sitecore.Support.164213
+                url: "/api/sitecore/SupportExportFormData/Export?id=" + id + "&format=" + format,
+                //End of changes
+                data: JSON.stringify({ "id": id }),
+                contentType: 'application/json',
+                dataType: 'json',
+                success: function (returnValue) {
+                    window.location = "/api/sitecore/ExportFormData/Download?file=" + returnValue.File + "&format=" + format;
+                }
+            });
+        }
+    });
+    return app;
+});
---
+++ src/Sitecore.Support.164213/WFFM/Services/Requests/Controllers/ExportFormDataController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Diagnostics;
+using Sitecore.Data.Items;
+using Sitecore.Form.Core.Configuration;
+using Sitecore.Jobs;
+using Sitecore.Pipelines;
+using Sitecore.WFFM.Analytics.Model;
+using Sitecore.WFFM.Services.Pipelines;
+using Sitecore.WFFM.Speak.ViewModel;
+using Sitecore.WFFM.Core.Resources;
+using Sitecore.Web.Http.Filters;
+using Sitecore.Forms.Core.Data;
+using Sitecore.WFFM.Analytics.Providers;
+using Sitecore.Configuration;
+using Sitecore.Data;
+
+namespace Sitecore.Support.WFFM.Services.Requests.Controllers
+{
+    public class SupportExportFormDataController: Controller
+    {
+        public SupportExportFormDataController() : this((IWfmDataProvider)Factory.CreateObject("wffm/formsDataProvider", true))
+        {
+        }
+
+        public SupportExportFormDataController(IWfmDataProvider formsDataProvider)
+        {
+            Assert.ArgumentNotNull(formsDataProvider, "formsDataProvider");
+            this.FormsDataProvider = formsDataProvider;
+        }
+        public IWfmDataProvider FormsDataProvider
+        {
+            get;
+            private set;
+        }
+
+        [ValidateHttpAntiForgeryToken]
+        public ActionResult Export(Guid id, int format)
+        {
+            Assert.ArgumentNotNull(id, "id");
+            Assert.ArgumentNotNull(format, "format");
+            bool flag = format == 0;
+            Item item = StaticSettings.MasterDatabase.GetItem(new ID(id));
+            Assert.IsTrue(item != null, "Can't find the form.");
+            Job job = Context.Job;
+            if (job != null)
+            {
+                job.Status.LogInfo(ResourceManager.Localize("READING_DATA_FROM_DATABASE"));
+            }
+            IEnumerable<IFormData> formData = this.FormsDataProvider.GetFormData(id);
+            FormExportArgs formExportArgs = new FormExportArgs(new FormItem(item), new FormPacket(formData), Form.Core.Utility.WebUtil.GetTempFileName(), flag ? "text /xml" : "application/vnd.ms-excel");
+            formExportArgs.Parameters.Add("contextUser", Context.User.Name);
+            CorePipeline.Run(flag ? "exportToXml" : "exportToExcel", formExportArgs);
+            return base.Json(new
+            {
+                //Sitecore.Support.164213
+                File = HttpContext.Server.UrlEncode(formExportArgs.FileName)
+                //End of changes
+            }, JsonRequestBehavior.AllowGet);
+        }
+        [ValidateHttpAntiForgeryToken]
+        public ActionResult Download(string file, int format)
+        {
+            Assert.ArgumentNotNullOrEmpty(file, "fileName");
+            string contentType = (format == 0) ? "text/xml" : "application/vnd.ms-excel";
+            string fileDownloadName = (format == 0) ? "export.xml" : "export.xls";
+            return this.File(file, contentType, fileDownloadName);
+        }
+    }
+}
